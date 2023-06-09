266c1fe20f0b6fc4f1d418542febe944beafa56b
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-06-10 01:57:54 +0600

    post buy blood

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     // console.log("database connect");
     const userCollection = client.db('blood-bank').collection('user');
     const bloodsCollection = client.db('blood-bank').collection('bloods');
+    const buyBloodsCollection = client.db('blood-bank').collection('buyBloods');
     //   // // // // // // // // // // // //
 
     // create and update User
@@ -55,6 +56,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    // // all User filter by email category
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const cursor = userCollection.find(query);
+      const user = await cursor.toArray();
+      res.send(user);
+    });
     // get blood
     app.get('/bloods', async (req, res) => {
       const query = {};
@@ -69,6 +78,55 @@ async function run() {
       const result = await bloodsCollection.findOne(query);
       res.send(result);
     });
+    // restock blood item and update
+    app.put('/bloodId/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateQuantity = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          quantity: updateQuantity.quantity,
+        },
+      };
+      const result = await bloodsCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //                     Buy    //
+    // post buy
+    app.post('/buyBlood', async (req, res) => {
+      const postResult = req.body;
+      const result = await buyBloodsCollection.insertOne(postResult);
+      res.send(result);
+    });
+
+    // // Get all Notice
+    // app.get('/notices', async (req, res) => {
+    //   const query = {};
+    //   const cursor = noticeCollection.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // });
+    // // Notices Filter by email
+    // app.get('/notice/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { email };
+    //   const cursor = noticeCollection.find(query);
+    //   const user = await cursor.toArray();
+    //   res.send(user);
+    // });
+    // // // Delete one Notice
+    // app.delete('/notices/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await noticeCollection.deleteOne(query);
+    //   res.send(result);
+    // });
   } finally {
   }
 }
