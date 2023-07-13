61078e67fde62d59b196e24731646224c92cbab6
Dipankar Halder <dipankar15-11137@diu.edu.bd>
2023-07-14 01:58:47 +0600

    delete contact

--- index.js
+++ index.js
@@ -211,187 +211,21 @@ async function run() {
       res.send(result);
     });
 
-    // // Get all Buy
-    // app.get('/buyBlood', async (req, res) => {
-    //   const query = {};
-    //   const cursor = buyBloodsCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
+    // Get all contact
+    app.get('/contact', async (req, res) => {
+      const query = {};
+      const cursor = contactCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    // // restock blood item and update
-    // app.put('/userId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateFreeBlood = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       freeBlood: updateFreeBlood.freeBlood,
-    //     },
-    //   };
-    //   const result = await userCollection.updateOne(query, updateDoc, options);
-    //   res.send(result);
-    // });
-    // // get blood
-    // app.get('/bloods', async (req, res) => {
-    //   const query = {};
-    //   const cursor = bloodsCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-    // // get blood by id
-    // app.get('/blood/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await bloodsCollection.findOne(query);
-    //   res.send(result);
-    // });
-
-    // // restock blood item and update
-    // app.put('/bloodId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateQuantity = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       quantity: updateQuantity.quantity,
-    //     },
-    //   };
-    //   const result = await bloodsCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-
-    // //                     Buy    //
-    // // post buy
-    // app.post('/buyBlood', async (req, res) => {
-    //   const postResult = req.body;
-    //   const result = await buyBloodsCollection.insertOne(postResult);
-    //   res.send(result);
-    // });
-
-    // // Get all Buy
-    // app.get('/buyBlood', async (req, res) => {
-    //   const query = {};
-    //   const cursor = buyBloodsCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-    // // get buy blood by id
-    // app.get('/buyBloodBuyId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await buyBloodsCollection.findOne(query);
-    //   res.send(result);
-    // });
-    // // get buy filter by email
-    // app.get('/buyBlood/:category', async (req, res) => {
-    //   const email = req.params.email;
-    //   const query = { email };
-    //   const cursor = buyBloodsCollection.find(query);
-    //   const user = await cursor.toArray();
-    //   res.send(user);
-    // });
-
-    // // restock buy blood item and update delivered
-    // app.put('/buyBloodId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateDelivered = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       delivered: updateDelivered.delivered,
-    //     },
-    //   };
-    //   const result = await buyBloodsCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // restock buy blood item and update payment
-    // app.put('/buyBloodPayment/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatePayment = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       payment: updatePayment.payment,
-    //     },
-    //   };
-    //   const result = await buyBloodsCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // // Delete one buy blood
-    // app.delete('/buyBlood/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await buyBloodsCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-    // //donate blood update and put data
-    // app.put('/donateBlood/:email', async (req, res) => {
-    //   const email = req.params.email;
-    //   const user = req.body;
-
-    //   const filter = { email: email };
-    //   const options = { upsert: true };
-
-    //   const updatedDoc = {
-    //     $set: user,
-    //   };
-
-    //   const result = await donateBloodCollection.updateOne(
-    //     filter,
-    //     updatedDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // Get all donate blood
-    // app.get('/donateBlood', async (req, res) => {
-    //   const query = {};
-    //   const cursor = donateBloodCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-    // // restock donate blood and update accept
-    // app.put('/donateBloodId/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updateAccept = req.body;
-    //   const query = { _id: ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       accept: updateAccept.accept,
-    //     },
-    //   };
-    //   const result = await donateBloodCollection.updateOne(
-    //     query,
-    //     updateDoc,
-    //     options
-    //   );
-    //   res.send(result);
-    // });
-    // // // Delete one donate blood
-    // app.delete('/donateBlood/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await donateBloodCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    // Delete Buy contact
+    app.delete('/contact/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await contactCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
